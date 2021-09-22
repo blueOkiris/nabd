7f4155f3325a55397bba9aa0b6f8b94a8abe294e
Dylan Turner <dylantdmt@gmail.com>
2021-09-22 15:23:48 -0500

    Update Variable.cpp

--- Variable.cpp
+++ Variable.cpp
@@ -9,6 +9,7 @@
 #include <utility>
 #include <sstream>
 #include <vector>
+#include <any>
 #include <Variable.hpp>
 
 using namespace nabd;
@@ -16,18 +17,24 @@ using namespace nabd;
 template<class T1, class T2>
 Variable::Variable(
         const VariableType varType,
-        const std::vector<Variable<T1>> &first, const Variable<T2> &second) :
+        const std::vector<T1> &first, const T2 &second) :
         type(varType), value(first), value2(second) {
 }
 
-std::string Variable::toStr(void) const {
+Variable<std::string> Variable::toStr(void) const {
     switch(type) {
         case VariableType.String:
-            return first[0];
+            return *this;
         case VariableType.Number:
-            return std::to_string(first[0]);
+            return Variable<std::string, int>(
+                VariableType.String,
+                std::to_string(value[0]), 0
+            );
         case VariableType.Tuple:
-            return "(" + first[0].toStr() + ", " + second.toStr() + ")";
+            return Variable<std::string, int>(
+                VariableType.String,
+                "(" + value[0].toStr() + ", " + value2.toStr() + ")", 0
+            );
         case VariableType.List: {
             std::stringstream listRep;
             listRep << "{";
@@ -38,7 +45,10 @@ std::string Variable::toStr(void) const {
                 }
             }
             listRep << "}";
-            return listRep.str();
+            return Variable<std::string, int>(
+                VariableType.String,
+                listRep.str(), 0
+            );
         }
     }
 }
@@ -46,27 +56,64 @@ std::string Variable::toStr(void) const {
 double Variable::toNum(void) const {
     switch(type) {
         case Variable.String:
-            return first[0].length() > 0 ? static_cast<double>(first[0][0]) : 0;
+            return Variable<double, int>(
+                VariableType.Number,
+                value[0].length() > 0 ? static_cast<double>(value[0][0]) : 0, 0
+            );
         case Variable.Number:
-            return first[0];
+            return *this;
         case Variable.Tuple:
         case Variable.List:
-            return first[0].toNum();
+            return Variable<double, int>(
+                VariableType.Number,
+                value[0].toNum(), 0
+            );
     }
 }
 
-template<class T1, class T2>
-std::string Variable::toList(void) const {
+template<class OtherT>
+std::string Variable::toLs(const VariableType subType) const {
+    std::vector<OtherT> newData;
     switch(type) {
         case VariableType.String:
         case VariableType.Number:
         case VariableType.Tuple:
-            return Variable(
-                VariableType.List,
-                std::vector<Variable<T1, T2>>({ *this }),
-                std::vector<int>({ 0 })
-            );
+            switch(subType) {
+                case Variable.String:
+                    newData.push_back(toStr());
+                    break;
+                case Variable.Number:
+                    newData.push_back(toNum());
+                    break;
+                case Variable.Tuple:
+                    newData.push_back(toTup());
+                    break;
+                case Variable.List:
+                    newData.push_back(toLs(type));
+                    break;
+            }
+            break;
         case VariableType.List:
-            return *this;
+            for(const auto &datum : value) {
+                switch(subType) {
+                    case Variable.String:
+                        newData.push_back(datum.toStr());
+                        break;
+                    case Variable.Number:
+                        newData.push_back(datum.toNum());
+                        break;
+                    case Variable.Tuple:
+                        newData.push_back(datum.toTup());
+                        break;
+                    case Variable.List:
+                        newData.push_back(datum.toLs());
+                        break;
+                }
+            }
+            break;
     }
+    return Variable<OtherT, int>(
+        VariableType.List,
+        newData, 0
+    );
 }
