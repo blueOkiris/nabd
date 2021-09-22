1f0f20e66cc652f18c8be7c9ccc3e01321b1c864
Dylan Turner <dylantdmt@gmail.com>
2021-09-22 15:52:57 -0500

    Update Variable.cpp

--- Variable.cpp
+++ Variable.cpp
@@ -16,23 +16,23 @@ using namespace nabd;
 
 template<class T1, class T2>
 Variable::Variable(
-        const VariableType varType,
+        const VariableType varType, const VariableType subVarType,
         const std::vector<T1> &first, const T2 &second) :
-        type(varType), value(first), value2(second) {
+        type(varType), subType(subVarType), value(first), value2(second) {
 }
 
-Variable<std::string> Variable::toStr(void) const {
+Variable<std::string, int> Variable::toStr(void) const {
     switch(type) {
         case VariableType.String:
             return *this;
         case VariableType.Number:
             return Variable<std::string, int>(
-                VariableType.String,
+                VariableType.String, VariableType.String,
                 std::to_string(value[0]), 0
             );
         case VariableType.Tuple:
             return Variable<std::string, int>(
-                VariableType.String,
+                VariableType.String, VariableType.String,
                 "(" + value[0].toStr() + ", " + value2.toStr() + ")", 0
             );
         case VariableType.List: {
@@ -46,74 +46,53 @@ Variable<std::string> Variable::toStr(void) const {
             }
             listRep << "}";
             return Variable<std::string, int>(
-                VariableType.String,
+                VariableType.String, VariableType.String,
                 listRep.str(), 0
             );
         }
     }
 }
 
-double Variable::toNum(void) const {
+Variable<double, int> Variable::toNum(void) const {
     switch(type) {
         case Variable.String:
             return Variable<double, int>(
-                VariableType.Number,
+                VariableType.Number, VariableType.Number,
                 value[0].length() > 0 ? static_cast<double>(value[0][0]) : 0, 0
             );
         case Variable.Number:
             return *this;
         case Variable.Tuple:
         case Variable.List:
-            return Variable<double, int>(
-                VariableType.Number,
-                value[0].toNum(), 0
-            );
+            return value[0].toNum();
     }
 }
 
+/*
+ * This is complex because of this problem:
+ * How to convert a, say, string to a list of list of ints or something?
+ * Well, first we want to convert the string to an num
+ *  - num(strVar)
+ *  - toNum()
+ * Then we want to package it:
+ *  - [ num(strVar) ]
+ *  - Variable<double, int>(List, Number, std::vector<double>({ toNum().value[0]) }, 0)
+ * Then we want to package that:
+ *  - [ [ numberVar ] ]
+ *  - Variable<Variable<double, int>, int>(
+ *        List, List,
+ *        std::vector<Variable<double, int>({ toLs(Number)
+ * So how do we know to put a number there this time?
+ * We can't get the type of the template's template and check it's double, right?
+ * So we have to convert to list first, and then convert to the right kind of list
+ */
+
 template<class OtherT>
-std::string Variable::toLs(const VariableType subType) const {
-    std::vector<OtherT> newData;
-    switch(type) {
-        case VariableType.String:
-        case VariableType.Number:
-        case VariableType.Tuple:
-            switch(subType) {
-                case Variable.String:
-                    newData.push_back(toStr());
-                    break;
-                case Variable.Number:
-                    newData.push_back(toNum());
-                    break;
-                case Variable.Tuple:
-                    newData.push_back(toTup());
-                    break;
-                case Variable.List:
-                    newData.push_back(toLs(type));
-                    break;
-            }
-            break;
-        case VariableType.List:
-            for(const auto &datum : value) {
-                switch(subType) {
-                    case Variable.String:
-                        newData.push_back(datum.toStr());
-                        break;
-                    case Variable.Number:
-                        newData.push_back(datum.toNum());
-                        break;
-                    case Variable.Tuple:
-                        newData.push_back(datum.toTup());
-                        break;
-                    case Variable.List:
-                        newData.push_back(datum.toLs(datum.type));
-                        break;
-                }
-            }
-            break;
-    }
-    return Variable<OtherT, int>(
-        VariableType.List,
-        newData, 0
-    );
+Variable<T, int> Variable::toLs(const VariableType subType) {
+
+}
+
+template<class T1, class T2>
+Variable<T1, T2> Variable::_makeLs(void) const {
+    
 }
