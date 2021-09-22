74da7205952fc9a8c46cf0827dd6e54e79cfd683
Dylan Turner <dylantdmt@gmail.com>
2021-09-22 16:47:49 -0500

    Create HelloWorldTest.cpp

---
+++ HelloWorldTest.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: Dylan Turner
+ * Description: A program that should mimic the desired output of the compiler
+ */
+
+#include <vector>
+#include <string>
+#include <Variable.hpp>
+
+#include <std.hpp>
+
+using namespace nabd;
+
+VariablePointer
+
+int main(int argc, char **args) {
+    std::
