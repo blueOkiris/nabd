01b8f9071a06e63b4813602d1df67699f9472fb9
Dylan Turner <dylantdmt@gmail.com>
2021-09-22 16:27:24 -0500

    Update Variable.cpp

--- Variable.cpp
+++ Variable.cpp
@@ -9,93 +9,102 @@
 #include <utility>
 #include <sstream>
 #include <vector>
-#include <any>
+#include <memory>
 #include <Variable.hpp>
 
 using namespace nabd;
 
-template<class T1, class T2>
-Variable::Variable(
-        const VariableType varType, const VariableType subVarType,
-        const std::vector<T1> &first, const T2 &second) :
-        type(varType), subType(subVarType), value(first), value2(second) {
-}
-
-Variable<std::string, int> Variable::toStr(void) const {
-    switch(type) {
-        case VariableType.String:
-            return *this;
-        case VariableType.Number:
-            return Variable<std::string, int>(
-                VariableType.String, VariableType.String,
-                std::to_string(value[0]), 0
-            );
-        case VariableType.Tuple:
-            return Variable<std::string, int>(
-                VariableType.String, VariableType.String,
-                "(" + value[0].toStr() + ", " + value2.toStr() + ")", 0
-            );
+// All of them use a similar toList, so this is code reuse
+VariablePointer listHelper(
+        VariablePointer self, const std::vector<VariableType> &subTypes) {
+    switch(subTypes[0]) {
         case VariableType.List: {
-            std::stringstream listRep;
-            listRep << "{";
-            for(const auto &var : first) {
-                listRep << var.toStr();
-                if(&var != first.end() - 1) {
-                    listRep << ", ";
+            std::vector<VariableType> subSubTypes;
+            for(const auto &type : subTypes) {
+                if(type == *(subTypes.first())) {
+                    continue;
                 }
+                subSubTypes.push_back(type);
             }
-            listRep << "}";
-            return Variable<std::string, int>(
-                VariableType.String, VariableType.String,
-                listRep.str(), 0
-            );
+            
+            return std::make_shared<ListVariable>(std::vector<VariablePointer>(
+                self->toList(subSubTypes)
+            ));
         }
+        case VariableType.Number:
+            return std::make_shared<ListVariable>(std::vector<VariablePointer>(
+                self->toNumber()
+            ));
+        case VariableType.String:
+            return std::make_shared<ListVariable>(std::vector<VariablePointer>(
+                self->toString()
+            ));
+        case VariableType.Tuple:
+            return std::make_shared<ListVariable>(std::vector<VariablePointer>(
+                self->toTuple()
+            ));
     }
 }
 
-Variable<double, int> Variable::toNum(void) const {
-    switch(type) {
-        case Variable.String:
-            return Variable<double, int>(
-                VariableType.Number, VariableType.Number,
-                value[0].length() > 0 ? static_cast<double>(value[0][0]) : 0, 0
-            );
-        case Variable.Number:
-            return *this;
-        case Variable.Tuple:
-        case Variable.List:
-            return value[0].toNum();
-    }
+
+StringVariable::StringVariable(const std::string &data) : value(data) {
 }
 
-/*
- * This is complex because of this problem:
- * How to convert a, say, string to a list of list of ints or something?
- * Well, first we want to convert the string to an num
- *  - num(strVar)
- *  - toNum()
- * Then we want to package it:
- *  - [ num(strVar) ]
- *  - Variable<double, int>(List, Number, std::vector<double>({ toNum().value[0]) }, 0)
- * Then we want to package that:
- *  - [ [ numberVar ] ]
- *  - Variable<Variable<double, int>, int>(
- *        List, List,
- *        std::vector<Variable<double, int>({ toLs(Number)
- * So how do we know to put a number there this time?
- * We can't get the type of the template's template and check it's double, right?
- * So we have to convert to list first, and then convert to the right kind of list
- */
-
-template<class OtherT>
-Variable<T, int> Variable::toLs(const VariableType subType) {
-
+VariablePointer StringVariable::toString(void) const {
+    return std::make_shared<StringVariable>(value);
 }
 
-template<class T1, class T2>
-Variable<T1, T2> Variable::_makeLs(void) const {
-    return Variable<T1, T2>(
-        VariableType.List, VariableType.String,
-        std::vector<Variable<T1, T2>>({ *this }), 0
+VariablePointer StringVariable::toNumber(void) const {
+    return std::make_shared<NumberVariable>(
+        value.length() > 0 ? static_cast<double>(value[0]) : 0
     );
 }
+
+VariablePointer StringVariable::toList(
+        const std::vector<VariableType> &subTypes) const {
+    return listHelper(std::shared_ptr(this), subTypes);
+}
+
+VariablePointer StringVariable::toTuple(void) const {
+    return std::make_shared<TupleVariable>(std::make_pair(
+        std::shared_ptr(this), std::shared_ptr(this)
+    ));
+}
+
+/*struct NumberVariable : public Variable {
+    public:
+        NumberVariable(const double data);
+        
+        VariablePointer toString(void) const override;
+        VariablePointer toNumber(void) const override;
+        VariablePointer toList(void) const override;
+        VariablePointer toTuple(void) const override;
+        
+        const double value;
+};
+
+struct ListVariable : public Variable {
+    public:
+        ListVariable(const std::vector<VariablePointer> &data);
+        
+        VariablePointer toString(void) const override;
+        VariablePointer toNumber(void) const override;
+        VariablePointer toList(void) const override;
+        VariablePointer toTuple(void) const override;
+        
+        const std::vector<VariablePointer> values;
+};
+
+struct TupleVariable : public Variable {
+    public:
+        TupleVariable(
+            const std::pair<VariablePointer, VariablePointer> &data
+        );
+        
+        VariablePointer toString(void) const override;
+        VariablePointer toNumber(void) const override;
+        VariablePointer toList(void) const override;
+        VariablePointer toTuple(void) const override;
+        
+        const std::pair<VariablePointer, VariablePointer> values;
+};*/
