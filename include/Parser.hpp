3f3216aaf0c7604a68a79d2ec1505b820e0d266b
Dylan Turner <dylantdmt@gmail.com>
2021-09-23 21:37:33 -0500

    Started on parser by defining EBNF for nabd

--- include/Parser.hpp
+++ include/Parser.hpp
@@ -1,2 +1,59 @@
+/*
+ * Author: Dylan Turner
+ * Description: Tool for extracting meaning from nabd source code
+ */
+
 #pragma once
 
+/*
+ * EBNF for nabd
+ *
+ * <include> ::=        <dol-sign> <identifier> <dol-sign>
+ * <func-def> ::=       <identifier> <equ-sign> <identifier> <rarr>
+ *                      <expr> <period>
+ * <func-call> ::=      <identifier> <lpar> <expr> <rpar>
+ * <ternary> ::=        <expression> <q-mark> <expr> <colon> <expr>
+ * 
+ * <list-def> ::=       <lbrak> <type> <type-op> { <exp> { <comma> <expr> } }
+ * <tup-def> ::=        <lcurl> <type> <type-op> <expr> <comma>
+ *                      <type> <type-op> <expr> <rcurl>
+ * 
+ * <expr> ::=           <func-call> | <ternary> |
+ *                      <string> | <decimal> | <hex> |
+ *                      <tup-def> | <list-def>
+ * 
+ * <type> ::=           'Str' | 'Num' |
+ *                      <lbrak> <type> <rbrak> |
+ *                      <lcurl> <type> <comma> <type> <rcurl>
+ * 
+ * <dol-sign> ::=       '$'
+ * <equ-sign> ::=       '='
+ * <period> ::=         '.'
+ * <rarr> ::=           '>'
+ * <lpar> ::=           '('
+ * <rpar> ::=           ')'
+ * <q-mark> ::=         '?'
+ * <colon> ::=          ':'
+ * <lbrak> ::=          '['
+ * <rbrak> ::=          ']'
+ * <lcurl> ::=          '{'
+ * <rcurl> ::=          '}'
+ * <comma> ::=          ','
+ * <type-op> ::=        ':>'
+ * 
+ * <decimal> ::=        /0d[0-9]+(\.[0-9])*#/
+ * <hex> ::=            /0x[0-9a-fA-F]+#/
+ * <string> ::=         /'(\\.|[^\\'])*'
+ * <identifier> ::=     /[A-Za-z_][A-Za-z_0-9]+/
+ */
+
+namespace nabd {
+    enum class TokenType {
+        Include, FuncDef,
+        FuncCall, Ternary, ListDef, TupDef, Expr,
+        Type,
+        DolSign, EquSign, Period, RArr, LPar, RPar, QMark, Colon, LBrak, RBrak,
+        LCurl, RCurl, Comma, TypeOp,
+        Decimal, Hex, String, Identifier
+    };
+}
--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,1 +1,10 @@
+/*
+ * Author: Dylan Turner
+ * Description: Implementation of tool for extracting meaning from nabd source
+ */
+
+#include <Parser.hpp>
+
+using namespace nabd;
+
 
